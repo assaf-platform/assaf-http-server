34b47c03482ea454b281e77205c2c0c682d0b244
Raam Rosh Hai <raam.roshhai@wonderkind.com>
2019-04-19 12:24:47 +0200

    clean up

--- cmd/assaf-http/main.go
+++ cmd/assaf-http/main.go
@@ -39,16 +39,6 @@ var (
 
 // initJaeger returns an instance of Jaeger Tracer that samples 100% of traces and logs all spans to stdout.
 func initJaeger(service string) (opentracing.Tracer, io.Closer) {
-	//cfg := &config.Configuration{
-	//	Sampler: &config.SamplerConfig{
-	//		Type:  "const",
-	//		Param: 1,
-	//	},
-	//	Reporter: &config.ReporterConfig{
-	//		LogSpans: true,
-	//	},
-	//	ServiceName: service,
-	//}
 	cfg, err := config.FromEnv()
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
@@ -57,8 +47,7 @@ func initJaeger(service string) (opentracing.Tracer, io.Closer) {
 	cfg.Sampler.Param = 1
 	cfg.ServiceName = service
 	cfg.Reporter.LogSpans = true
-	log.Print("#######################")
-	log.Print(cfg.Reporter.LocalAgentHostPort)
+	log.Printf("agent host:port = %s", cfg.Reporter.LocalAgentHostPort)
 
 	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
 	if err != nil {
